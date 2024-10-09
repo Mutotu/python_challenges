6e4f02ad9ae8942f1e9a4d8ee051705bc1af8fc6
Muttalip Aydemir <36741298+Mutotu@users.noreply.github.com>
2024-10-09 20:52:30 +0100

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,95 @@
+class BankAccount:
+
+    def __init__(self):
+        self.balance = 0
+        self.interest_rate = 0.02
+
+    def deposit(self, amount):
+        if amount < 0:
+            return False
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self,amount):
+        if amount <= 0 or amount > self.balance:
+            return False
+        self.balance -= amount
+        return self.balance
+
+    def accumulate_interest(self):
+       self.balance += (self.interest_rate * self.balance)
+       return self.balance
+
+
+class ChildrensAccount(BankAccount):
+
+    def __init__(self):
+        super().__init__()
+        self.interest_rate = 0
+
+    def accumulate_interest(self):
+       self.balance += 10
+       return self.balance
+
+class OverdraftAccount(BankAccount):
+    def __init__(self):
+        super().__init__()
+        self.overdraft_penalty = 40
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            return False
+
+        if amount > self.balance:
+            self.balance -= self.overdraft_penalty
+            return False
+        else:
+            self.balance -= amount
+            return self.balance
+
+    def accumulate_interest(self):
+        if self.balance < 0:
+            return False
+        return super().accumulate_interest()
+
+try:
+    basic_account = BankAccount()
+    basic_account.deposit(600)
+    print("Basic account has ${}".format(basic_account.balance))
+    basic_account.withdraw(17)
+    print("Basic account has ${}".format(basic_account.balance))
+    basic_account.accumulate_interest()
+    print("Basic account has ${}".format(basic_account.balance))
+    print()
+except Exception as e:
+    print(e)
+
+print()
+print()
+print()
+try:
+  childs_account = ChildrensAccount()
+  childs_account.deposit(34)
+  print("Child's account has ${}".format(childs_account.balance))
+  childs_account.withdraw(17)
+  print("Child's account has ${}".format(childs_account.balance))
+  childs_account.accumulate_interest()
+  print("Child's account has ${}".format(childs_account.balance))
+  print()
+except Exception as e:
+  print(e)
+
+print()
+print()
+print()
+
+try:
+  overdraft_account = OverdraftAccount()
+  overdraft_account.deposit(12)
+  print("Overdraft account has ${}".format(overdraft_account.balance))
+  overdraft_account.withdraw(17)
+  print("Overdraft account has ${}".format(overdraft_account.balance))
+  overdraft_account.accumulate_interest()
+  print("Overdraft account has ${}".format(overdraft_account.balance))
+except Exception as e:
+  print(e)
